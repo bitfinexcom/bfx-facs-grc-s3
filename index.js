ff2aa7799dbefe604eb73317a58648e3c0f5c2fd
batrudinych <batrudinych@gmail.com>
2026-01-23 17:10:45 +0000

    update name and deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Base = require('bfx-facs-base')
+const Base = require('@bitfinex/bfx-facs-base')
 const { getAsciiFileName } = require('./helpers')
 
 class S3Grc extends Base {
